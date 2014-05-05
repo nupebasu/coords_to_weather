9bfed1ffbd4a9e32d149b23217ce8f36614d3c21
Anupam Basu <nupebasu@gmail.com>
2014-05-04 20:51:56 -0500

    v1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 CoordsToWeather::Application.routes.draw do
   get("/weather", { :controller => "coords", :action => "fetch_weather" })
+  get("/weather/:sign", { :controller => "coords", :action => "fetch_weather" })
+
 end
